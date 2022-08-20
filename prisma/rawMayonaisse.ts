457cd49b2dfa598076488a62df4a6b610ab465a4
sigristarisa <arisasigrist.ch@gmail.com>
2022-08-20 15:50:54 +0200

    created rawdish.ts

---
+++ prisma/rawMayonaisse.ts
@@ -0,0 +1,13 @@
+type Dish = {
+  name: string;
+};
+
+export const rawDishes: Dish[] = [
+  { name: "vegetable" },
+  { name: "egg" },
+  { name: "meat" },
+  { name: "seafood" },
+  { name: "bread" },
+  { name: "rice" },
+  { name: "snack" },
+];
