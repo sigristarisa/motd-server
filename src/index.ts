7f78bf162baf26e63c4c1a14191852ef6294c986
sigristarisa <arisasigrist.ch@gmail.com>
2022-08-28 17:09:28 +0200

    modified cors

--- src/index.ts
+++ src/index.ts
@@ -12,9 +12,6 @@ app.use(express.static("mayo-image"));
 app.use(express.static("dish-image"));
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "http://localhost:4000");
-  res.header("Access-Control-Allow-Methods", "GET, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
 });
 
 app.use("/assets", express.static("assets"));
