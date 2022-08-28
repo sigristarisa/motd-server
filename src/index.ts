38de9418f0b4e41d75858fc92b00d238a9e2cbcf
sigristarisa <arisasigrist.ch@gmail.com>
2022-08-28 15:43:10 +0200

    turned off cors

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
 import express from "express";
-import cors from "cors";
+// import cors from "cors";
 import morgan from "morgan";
 import mayonnaiseRouter from "./routes/mayonnaise";
 
 const app = express();
-app.use(cors());
+// app.use(cors());
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("assets"));
 app.use(express.static("mayo-image"));
 app.use(express.static("dish-image"));
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:4000");
-  res.header("Access-Control-Allow-Methods", "GET, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "http://localhost:4000");
+//   res.header("Access-Control-Allow-Methods", "GET, OPTIONS");
+//   res.header("Access-Control-Allow-Headers", "Content-Type");
+//   next();
+// });
 
 app.use("/assets", express.static("assets"));
 app.use("/mayonnaise", mayonnaiseRouter);
