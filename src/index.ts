e81b74d60de8a7169515f9757b35269390f9424f
sigristarisa <arisasigrist.ch@gmail.com>
2022-11-24 17:48:25 +0100

    set access control allow origin

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,14 @@ app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("assets"));
 app.use(express.static("dish-image"));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use("/assets", express.static("assets"));
 app.use("/mayonnaise", mayonnaiseRouter);
